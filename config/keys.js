3dec0092a0c53d9e1eb5ca3d556b8b8b43241036
Damon <ccmxc1991@gmail.com>
2018-11-21 07:01:38 +1100

    Complete basic backend and part of frontend

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://Damon:dian1393@ds135433.mlab.com:35433/damontest'
+    mongoURI: 'mongodb://Damon:dian1393@ds135433.mlab.com:35433/damontest',
+    secretOrKey: 'secret'
 }
